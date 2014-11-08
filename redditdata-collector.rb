ee25da17bc71745050ddae9dfb95c1e0e93f0b2c
mogitfamcap <mogitfamcap@gmail.com>
2014-11-08 13:03:15 -0800

    process datasets separately

--- redditdata-collector.rb
+++ redditdata-collector.rb
@@ -8,21 +8,27 @@ require './schema.rb'
 
 puts 'redditdata-collector has started'
 
-if (ARGV.size != 3) then
-  puts 'Usage: reddit-collector PATH_TO_DATABASE MODE SUBREDDIT_REGEX'
+if (ARGV.size != 4) then
+  puts 'Usage: reddit-collector PATH_TO_DATABASE DATASET MODE SUBREDDIT_REGEX'
   exit 1
 end
 
 path_to_database = ARGV[0]
-mode = ARGV[1]
-subreddit_regex = ARGV[2]
+dataset = ARGV[1]
+mode = ARGV[2]
+subreddit_regex = ARGV[3]
 
 sql_client = SqlClient::create(path_to_database, mode)
-subreddit_collector = SubredditCollector.new sql_client
-link_collector = LinkCollector.new sql_client
 
-subreddit_collector.collect mode
-link_collector.collect(mode, subreddit_regex)
-
+case dataset
+  when 'subreddits'
+    subreddit_collector = SubredditCollector.new sql_client
+    subreddit_collector.collect mode
+  when 'links'
+    link_collector = LinkCollector.new sql_client
+    link_collector.collect(mode, subreddit_regex)
+  else
+    puts "dataset is not supported: #{dataset}"
+end
 
 puts 'redditdata-collector has finished'
