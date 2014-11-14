aaa94bacd93458fd734a6ccf5965ce50ee9bd3ab
mogitfamcap <mogitfamcap@gmail.com>
2014-11-14 00:42:49 -0800

    move private methods to the bottom

--- lib/redditdata-collector/sql_client.rb
+++ lib/redditdata-collector/sql_client.rb
@@ -13,77 +13,6 @@ class SqlClient
     @mode = mode
   end
 
-  def setup_tables(dataset, mode)
-    Util.log 'Setting up tables'
-
-    if mode == 'bootstrap'
-      Util.log "Dropping table #{dataset}"
-      @db.execute "DROP TABLE IF EXISTS #{dataset} ;"
-    end
-
-    Util.log "Creating table #{dataset}"
-    q = get_create_statement(dataset, Schema.get_schema_for_dataset(dataset))
-    @db.execute(q)
-
-    Util.log 'Setting up tables competed'
-  end
-
-  def get_create_statement(name, schema)
-    res = "CREATE TABLE IF NOT EXISTS #{name} ("
-    schema.each do |schema_element|
-      res += schema_element[:name] + ' ' + (schema_element[:type] != Schema::TYPE_BOOLEAN ? schema_element[:type] : Schema::TYPE_INTEGER)
-      res += schema_element[:primary_key?] ? ' PRIMARY KEY' : ''
-      res += ', '
-    end
-    res += 'updated_at REAL'
-    res += ');'
-    res
-  end
-
-  def get_insert_statement(name, schema)
-    res = "INSERT INTO #{name} VALUES ("
-    schema.each do |schema_element|
-      res += '?, '
-    end
-    res += '?);'
-    res
-  end
-
-  def get_update_statement(name, schema)
-    res = "UPDATE #{name} SET "
-    schema.each do |schema_element|
-      res += schema_element[:name] + ' = ?, '
-    end
-    res += 'updated_at = ? '
-    res += 'WHERE ' + Schema.get_primary_key_name(schema) + '= ?;'
-    res
-  end
-
-  def get_insert_values(schema, object)
-    result = []
-    schema.each do |schema_element|
-      case schema_element[:type]
-        when Schema::TYPE_TEXT
-          result.push object.attributes[schema_element[:name].to_sym].to_s
-        when Schema::TYPE_INTEGER
-          result.push object.attributes[schema_element[:name].to_sym]
-        when Schema::TYPE_REAL
-          result.push object.attributes[schema_element[:name].to_sym].to_i
-        when Schema::TYPE_BOOLEAN
-          result.push(object.attributes[schema_element[:name].to_sym] ? 1 : 0)
-      end
-
-    end
-    result.push(Time.now.to_i)
-    result
-  end
-
-  def get_update_values(schema, object)
-    res = get_insert_values(schema, object)
-    res.push(Schema.get_primary_key_value(schema, object))
-    res
-  end
-
   def add_subreddit(subreddit, mode)
     if !exists?('subreddits', subreddit) then
       insert_item('subreddits', subreddit)
@@ -161,22 +90,6 @@ class SqlClient
     return
   end
 
-  def exists?(dataset, object)
-    schema = Schema.get_schema_for_dataset dataset
-    primary_key_name = Schema.get_primary_key_name schema
-    primary_key_value = Schema.get_primary_key_value(schema, object)
-
-    q = "SELECT #{primary_key_name} FROM #{dataset} WHERE #{primary_key_name} = ?;"
-    res = @db.execute(q, primary_key_value)
-    !res.empty?
-  end
-
-  def insert_item(dataset, object)
-    q = get_insert_statement(dataset, Schema.get_schema_for_dataset(dataset))
-    v = get_insert_values(Schema.get_schema_for_dataset(dataset), object)
-    @db.execute(q, v)
-  end
-
   def update_subreddit(subreddit)
     q = get_update_statement('subreddits', Schema.subreddit_schema)
     v = get_update_values(Schema.subreddit_schema, subreddit)
@@ -249,5 +162,94 @@ class SqlClient
     q_res = @db.execute(q)
 
     q_res.map { |row| row[0] }
+  end
+
+  private
+
+  def setup_tables(dataset, mode)
+    Util.log 'Setting up tables'
+
+    if mode == 'bootstrap'
+      Util.log "Dropping table #{dataset}"
+      @db.execute "DROP TABLE IF EXISTS #{dataset} ;"
+    end
+
+    Util.log "Creating table #{dataset}"
+    q = get_create_statement(dataset, Schema.get_schema_for_dataset(dataset))
+    @db.execute(q)
+
+    Util.log 'Setting up tables competed'
+  end
+
+  def get_create_statement(name, schema)
+    res = "CREATE TABLE IF NOT EXISTS #{name} ("
+    schema.each do |schema_element|
+      res += schema_element[:name] + ' ' + (schema_element[:type] != Schema::TYPE_BOOLEAN ? schema_element[:type] : Schema::TYPE_INTEGER)
+      res += schema_element[:primary_key?] ? ' PRIMARY KEY' : ''
+      res += ', '
+    end
+    res += 'updated_at REAL'
+    res += ');'
+    res
+  end
+
+  def get_insert_statement(name, schema)
+    res = "INSERT INTO #{name} VALUES ("
+    schema.each do |schema_element|
+      res += '?, '
+    end
+    res += '?);'
+    res
+  end
+
+  def get_update_statement(name, schema)
+    res = "UPDATE #{name} SET "
+    schema.each do |schema_element|
+      res += schema_element[:name] + ' = ?, '
+    end
+    res += 'updated_at = ? '
+    res += 'WHERE ' + Schema.get_primary_key_name(schema) + '= ?;'
+    res
+  end
+
+  def get_insert_values(schema, object)
+    result = []
+    schema.each do |schema_element|
+      case schema_element[:type]
+        when Schema::TYPE_TEXT
+          result.push object.attributes[schema_element[:name].to_sym].to_s
+        when Schema::TYPE_INTEGER
+          result.push object.attributes[schema_element[:name].to_sym]
+        when Schema::TYPE_REAL
+          result.push object.attributes[schema_element[:name].to_sym].to_i
+        when Schema::TYPE_BOOLEAN
+          result.push(object.attributes[schema_element[:name].to_sym] ? 1 : 0)
+      end
+
+    end
+    result.push(Time.now.to_i)
+    result
+  end
+
+  def get_update_values(schema, object)
+    res = get_insert_values(schema, object)
+    res.push(Schema.get_primary_key_value(schema, object))
+    res
+  end
+
+  def exists?(dataset, object)
+    schema = Schema.get_schema_for_dataset dataset
+    primary_key_name = Schema.get_primary_key_name schema
+    primary_key_value = Schema.get_primary_key_value(schema, object)
+
+    q = "SELECT #{primary_key_name} FROM #{dataset} WHERE #{primary_key_name} = ?;"
+    res = @db.execute(q, primary_key_value)
+    !res.empty?
+  end
+
+  def insert_item(dataset, object)
+    q = get_insert_statement(dataset, Schema.get_schema_for_dataset(dataset))
+    v = get_insert_values(Schema.get_schema_for_dataset(dataset), object)
+    @db.execute(q, v)
   end
 end
